5a5097c5cf4394855c6f53bfa5db243e09454ba5
srggroup <adam.balint@srg.hu>
2020-03-08 18:37:27 +0100

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=meet_db;dbname=meet',
+    'username' => 'meet',
+    'password' => 'foT7B1ENKZ',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
