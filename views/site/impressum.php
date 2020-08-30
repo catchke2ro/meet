31fdba20f133dd0c6ed77409971ffe6870bfb98f
adambalint-srg <adam.balint@srg.hu>
2020-08-30 20:21:52 +0200

    Impresszum fixes

--- views/site/impressum.php
+++ views/site/impressum.php
@@ -5,18 +5,15 @@ $this->title = 'Impresszum';
 
 
 <div class="stat impressum">
-	<h1 style="text-align: center;">Impresszum</h1>
-	<p>A Műveld és őrizd! Evangélikus Egyházi Teremtésvédelmi (MEET) program hivatalos honlapja a meet.lutheran.hu címen érhető el. A meet.lutheran.hu minden tartalma szerzői jogi védelem alatt áll. A honlapon elhelyezett szövegek a lapra és a szerzőkre való hivatkozással szabadon idézhetőek. A minden más tartalom felhasználásához azonban a Magyarországi Evangélikus Egyház Ararát Teremtésvédelmi Munkaágának előzetes, írásbeli engedélye szükséges.</p>
-	<p>A honlap tulajdonosa:<br />Magyarországi Evangélikus Egyház<br />Székhelye: 1085 Budapest, Üllői út 24.<br />Adószám: 19719483-2-42<br />Technikai szám: 0035 </p>
-	<p>A honlap üzemeltetője:<br />Magyarországi Evangélikus Egyház<br />Ararát Teremtésvédelmi Munkaága<br />Székhelye: 1085 Budapest, Üllői út 24.<br />Elérhetősége: arterm@lutheran.hu </p>
-	<p>A MEET program elérhetősége:<br />meet@lutheran.hu</p>
-	<p>A honlap főszerkesztője:<br />Koltai Zsuzsa, koordinátor<br />zsuzsa.koltai@lutheran.hu</p>
-	<p>A honlapon elhelyezett, megosztott ‘Aktivitás’ tartalmakért (hírek, események, meghívók, jó gyakorlatok) a megjelölt szerzők maguk felelősek. </p>
-	<p>A honlap és tervező program fejlesztője: Bálint Ádám, programozó<br />Képek, arculat és dizájn: Pete Dóra, grafikus<br />A képek felhasználása az alkotó nevének feltüntetésével és forrásmegjelöléssel lehetséges.<br />Az arculat és dizájn elemek felhasználása kizárólag az üzemeltető hozzájárulásával lehetséges. </p>
-	<p>A technikai hátteret a MEE Országos Iroda biztosítja, szabad szoftveres megoldásokra építve.<br />Projektfelelős: Bagi Tamás<br />tamas.bagi@lutheran.hu </p>
-	<p>Tárhely szolgáltató:<br />Magyarországi Evangélikus Egyház</p>
-	<p> </p>
-	<p> </p>
-
 	<img src="/assets/img/logos/meet_logo.png" alt="MEET logó" class="meetLogo"/>
+	<p>A <strong class="text-primary">Műveld és őrizd! Evangélikus Egyházi Teremtésvédelmi (MEET) program</strong> hivatalos honlapja a meet.lutheran.hu címen érhető el. A meet.lutheran.hu minden tartalma szerzői jogi védelem alatt áll. A honlapon elhelyezett szövegek a lapra és a szerzőkre való hivatkozással szabadon idézhetőek. A minden más tartalom felhasználásához azonban a Magyarországi Evangélikus Egyház Ararát Teremtésvédelmi Munkaágának előzetes, írásbeli engedélye szükséges.</p>
+	<p><strong class="text-primary">A honlap tulajdonosa:<br /></strong>Magyarországi Evangélikus Egyház<br />Székhelye: 1085 Budapest, Üllői út 24.<br />Adószám: 19719483-2-42<br />Technikai szám: 0035 </p>
+	<p><strong class="text-primary">A honlap üzemeltetője:<br /></strong>Magyarországi Evangélikus Egyház<br />Ararát Teremtésvédelmi Munkaága<br />Székhelye: 1085 Budapest, Üllői út 24.<br />Elérhetősége: arterm@lutheran.hu </p>
+	<p><strong class="text-primary">A MEET program elérhetősége:<br /></strong>meet@lutheran.hu</p>
+	<p><strong class="text-primary">A honlap főszerkesztője:<br /></strong>Koltai Zsuzsa, koordinátor<br />zsuzsa.koltai@lutheran.hu</p>
+	<p>A honlapon elhelyezett, megosztott ‘Aktivitás’ tartalmakért (hírek, események, meghívók, jó gyakorlatok) a megjelölt szerzők maguk felelősek. </p>
+	<p><strong class="text-primary">A honlap és tervező program fejlesztője:</strong> Bálint Ádám, programozó<br /><strong class="text-primary">Képek, arculat és dizájn:</strong> Pete Dóra, grafikus (<a href="https://www.behance.net/porah">https://www.behance.net/porah</a>)<br />A képek felhasználása az alkotó nevének feltüntetésével és forrásmegjelöléssel lehetséges.<br />Az arculat és dizájn elemek felhasználása kizárólag az üzemeltető hozzájárulásával lehetséges. </p>
+	<p>A technikai hátteret a MEE Országos Iroda biztosítja, szabad szoftveres megoldásokra építve.<br /><strong class="text-primary">Projektfelelős:</strong> Bagi Tamás<br />tamas.bagi@lutheran.hu </p>
+	<p><strong class="text-primary">Tárhely szolgáltató:<br /></strong>Magyarországi Evangélikus Egyház</p>
+
 </div>
