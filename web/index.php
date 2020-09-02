2403b50ebc2041e9407fb2a772b6d264f9409c12
adambalint-srg <adam.balint@srg.hu>
2020-09-02 19:24:00 +0200

    Prod mode default

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', !empty($_COOKIE['SRGDEV']));
-defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_ENV') or define('YII_ENV', !empty($_COOKIE['SRGDEV']) ? 'dev' : 'prod');
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
